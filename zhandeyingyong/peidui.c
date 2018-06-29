ee8f12afc817f12efc4b5f726404f1400ca1c1b0
yuebaixiao <wsyueshuai@163.com>
2018-06-29 18:07:17 +0800

    zhandeyingyong

--- zhandeyingyong/peidui.c
+++ zhandeyingyong/peidui.c
@@ -6,25 +6,44 @@ void pop(char** top){
   --(*top);
 }
 void push (char** top, char * base, char s){
+  switch (s){
+  case ']':
+    s = '[';
+    break;
+  case ')':
+    s = '(';
+    break;
+  case '}':
+    s = '{';
+    break;
+  default:
+    break;
+  }
+  //保存top的指向
   char* tmp = *top;
+  //第一次压栈，或者，s和栈顶的值不相同就继续压栈
   if(*top == base || s != *--tmp){
     *(*top)++ = s;
   }
+  //s和栈顶的值相同就弹出
   else{
     pop(top);
   }
 }
 int main(){
-  //char* s = "[([][])]";
-  char* s = "[]";
+  char* s = "[({}[])]";
+  //char* s = "[";
   char *top, *base;
   top = base = (char*)malloc(sizeof(char) * strlen(s));
   int i = 0;
   for(; i < strlen(s); ++i){
     push(&top, base, s[i]);
   }
-  if(top == base && s[i+1] == '\0'){
+  //如果栈顶又回到了栈底，就说明完全都匹配上了
+  if(top == base){
     printf("success\n");
+  }else{
+    printf("dead\n");
   }
   free(base);
 }
